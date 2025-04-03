28637959f0581ed7713b9a9c5c64a5a1d94a3cfb
ArthurMouraa <arthurmoura090@gmail.com>
2025-04-03 17:09:13 -0300

    Adicionando arquitetura base do backend

---
+++ src/main/java/com/sosfauna/sosfaunaBackend/SosfaunaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sosfauna.sosfaunaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosfaunaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosfaunaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sosfauna/sosfaunaBackend/SosfaunaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosfauna.sosfaunaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosfaunaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
